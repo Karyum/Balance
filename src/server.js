ee37e551cc00825fcff601cde67b2f1ba3d012a4
mario <mario.saliba98@gmail.com>
2017-12-12 10:19:47 +0200

    what the hell is going on

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.resolve(__dirname, '../index.html'))
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
 );
 
 app.listen(process.env.PORT || 3000, () => {
