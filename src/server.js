c7c49429a20d0a7c61734e7de5cc1d4ec5733031
mario <mario.saliba98@gmail.com>
2017-12-12 10:10:36 +0200

    change port

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) =>
   res.sendFile(path.resolve(__dirname, '../index.html'))
 );
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(3000);
 });
